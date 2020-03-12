634ac780868b9a6c1817e581ea0f17bcc7cf05f0
diogenesistemas <ads.diogenes@gmail.com>
2020-03-12 15:14:51 -0300

    Definindo Modelo Produto sem implementação

--- src/main/java/br/com/rd/exerciciospring/model/Produto.java
+++ src/main/java/br/com/rd/exerciciospring/model/Produto.java
@@ -1,8 +1,29 @@
 package br.com.rd.exerciciospring.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
 import java.io.Serializable;
 
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "TB_PRODUTO")
 public class Produto implements Serializable {
 
-    //TODO ID_PRODUTO Long (primary key) DS_PRODUTO String
+    //TODO ID_PRODUTO Long (primary key)
+    // DS_PRODUTO String
+
+    @Column(name = "ID_PRODUTO")
+    private Long idProduto;
+    @Column(name = "DS_PRODUTO")
+    private String descricao;
+
+
 }
